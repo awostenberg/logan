3c7ecab44f51d5ef264ca6c0a4e649dc3a9cce31
Alan Wostenberg <awosty@gmail.com>
2012-05-04 18:52:00 -0600

    simple cli using clojure.tools.cli

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "syslog log analysis"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-time "0.4.2"]
-                 [incanter "1.3.0"]]
+                 [incanter "1.3.0"]
+                 [org.clojure/tools.cli "0.2.1"]]
   :main logan.stuff)
 
 
--- src/logan/stuff.clj
+++ src/logan/stuff.clj
@@ -1,14 +1,39 @@
 (ns logan.stuff
   (:use [logan.core]
+        [clojure.tools.cli :only (cli)]
         [incanter core charts])
   (:gen-class))
 
 (defn plot-it [syslog-output-filename]
   (histogram (ordinals (syslog-dates syslog-output-filename)) :x-label (str "day in " syslog-output-filename)))
 
-(defn -main [& args]
+(defn oldmain [& args]
   (if (empty? args)
     (println "Usage: logan x.log
 \twhere x.log is output from osx 'syslog -u' command")
     (view (plot-it (first args)))))
+
+(defn all-lines
+  "lazy read STDIN (courtesy Gareth Jones)"
+  []
+  (lazy-cat
+   (when-let [line (read-line)]
+     (cons line (all-lines)))))
+
+(defn -main [& args]
+   (let [[options args banner]
+        (cli args
+             ["-h" "--help" "logan x.log (x.log is output of 'syslog -u')"
+              :default false :flag true])]
+
+     (when (or (:help options) (empty? args))
+      (println banner)
+      (System/exit 0))
+
+    (view (plot-it (first args)))))
+
+    
+
+
+
 
